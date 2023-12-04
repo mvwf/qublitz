30188176bf6958f2f0a7a0eae653811e3d6c1482
Mattias W. Fitzpatrick <f006dy5@t2783.kiewit.dartmouth.edu>
2023-12-04 13:54:18 -0500

    some debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import numpy as np
 import plotly.express as px
 from quantum_simulator import run_quantum_simulation, add_gaussian
+import plotly.figure_factory as ff
+
 
 def main():
     # ... Existing Streamlit code ...'
@@ -34,3 +36,21 @@ def main():
 
     # Display the generated plots
     st.plotly_chart(fig_simulation)
+
+
+    # Add histogram data
+    x1 = np.random.randn(200) - 2
+    x2 = np.random.randn(200)
+    x3 = np.random.randn(200) + 2
+
+    # Group data together
+    hist_data = [x1, x2, x3]
+
+    group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+    # Create distplot with custom bin_size
+    fig = ff.create_distplot(
+            hist_data, group_labels, bin_size=[.1, .25, .5])
+
+    # Plot!
+    st.plotly_chart(fig, use_container_width=True)
