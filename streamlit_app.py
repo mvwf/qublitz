8b2f1fbcf1c6b30d30f6f6029f34129da0b3d9b3
Mattias Fitzpatrick <110043624+mvwf@users.noreply.github.com>
2024-02-20 14:09:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,7 +95,8 @@ def main():
     if sim_mode == "Frequency Domain":
         start_freq = st.number_input(r"Start $\omega_d/2\pi$ [GHz]", value=4.8, step=0.1, key='start_freq_frequency_domain')
         stop_freq = st.number_input(r"Stop $\omega_d/2\pi$ [GHz]", value=5.2, step=0.1, key='stop_freq_frequency_domain')
-        num_points = st.number_input("Number of Frequencies", value=11, step=1, key='num_points_frequency_domain')
+        num_points = st.number_input("Number of Frequencies", value=11, min_value=1, max_value=101,  step=1, key='num_points_frequency_domain')
+        
         t_final = int(st.number_input(r'Duration $\Delta t$ [ns]', 0, value=25, step=1, key='t_final'))
         n_steps = 20*int(t_final)
 
