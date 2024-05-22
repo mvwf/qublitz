e0840c6b8aceab8404f6edd80972453bb67246bc
mvwf <mattias.w.fitzpatrick@dartmouth.edu>
2024-05-22 11:53:04 -0400

    forgot to add main function

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
-st.title ("Qublitz")
-st.write("Welcome to Qublitz! A quantum computing game where you can learn about quantum computing and quantum gates.")
-st.page_link("streamlit_app.py", label="Home", icon="🏠")
-st.page_link("pages/1_free_play.py", label="Free Play", icon="1️⃣")
-st.page_link("pages/2_gates_challenge.py", label="Gates Challenge", icon="2️⃣")
-st.page_link("pages/3_step_pulse_challenge.py", label="Step Pulse Challenge", icon="3️⃣")
-st.page_link("pages/4_custom_qubit_query.py", label="Custom Qubit Query", icon="4️⃣")
-st.page_link("pages/5_neutral_atoms.py", label=" Neutral Atoms", icon="🔬")
+def main():
+    st.title ("Qublitz")
+    st.write("Welcome to Qublitz! A quantum computing game where you can learn about quantum computing and quantum gates.")
+    st.page_link("streamlit_app.py", label="Home", icon="🏠")
+    st.page_link("pages/1_free_play.py", label="Free Play", icon="1️⃣")
+    st.page_link("pages/2_gates_challenge.py", label="Gates Challenge", icon="2️⃣")
+    st.page_link("pages/3_step_pulse_challenge.py", label="Step Pulse Challenge", icon="3️⃣")
+    st.page_link("pages/4_custom_qubit_query.py", label="Custom Qubit Query", icon="4️⃣")
+    st.page_link("pages/5_neutral_atoms.py", label=" Neutral Atoms", icon="🔬")
 
 if __name__ == "__main__":
     main()
