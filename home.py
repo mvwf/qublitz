76b78ea00344be265bb02802d31049d430931f30
Mattias Fitzpatrick <110043624+mvwf@users.noreply.github.com>
2025-08-07 19:40:21 -0400

    Update home.py
    changed filenames

--- home.py
+++ home.py
@@ -7,7 +7,7 @@ def main():
     st.write("Welcome to Qublitz!")
     st.page_link("home.py", label="Home", icon="🏠")
     st.page_link("pages/sonify.py", label="Sonify Images", icon="🎵")
-    st.page_link("pages/free_play.py", label="Qubit Simulator", icon="🎮")
+    st.page_link("pages/qubit_simulator.py", label="Qubit Simulator", icon="🎮")
     st.page_link("pages/custom_qubit_query.py", label="Custom Qubit Query", icon="🔬")
     st.page_link("pages/IQ_mixer.py", label="IQ Mixing", icon="〰️")
     st.page_link("pages/EP_TPD_exploration.py", label="Exceptional Point and Transmission Peak Degeneracy Exploration", icon="🎯")
